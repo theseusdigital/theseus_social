e58a72ef63a340f886f0a08ff1da4f9731b84e6c
nishant <nishantshetty92@gmail.com>
2017-11-21 20:19:44 +0530

    added shscripts for hashtracking scripts & added it to crontab

---
+++ shscripts/facebookhashtags.sh
@@ -0,0 +1,3 @@
+. /home/nishant/theseus_social/shscripts/common.sh
+$PY_EXEC tracker/facebook_hashtags.py
+deactivate
---
+++ shscripts/twitterhashtags.sh
@@ -0,0 +1,3 @@
+. /home/nishant/theseus_social/shscripts/common.sh
+$PY_EXEC tracker/twitter_hashtags.py
+deactivate
